00a445b53fa3546f94e3657982756713447fb010
Antonio Franca <antoniofranca571@gmail.com>
2025-05-19 19:36:24 -0400

    Atualizando os arquivos

--- questao_2.pl
+++ questao_2.pl
@@ -1,38 +1,44 @@
 % Definindo as probabilidades das variáveis
-% a condição da rua (str) pode ser dry, wet ou snow_covered
-0.81::str(dry).
-0.09::str(wet).
-0.1::str(snow_covered).
-% A variavel flw representa o volante do dinamo desgastado
-0.41::flw. % 0.59 é o complemento, a parte falsa que fica implicita
-% Probabilidades condicionais de R (dínamo deslizante) dadas Str
-% (condicão da rua) e Flw (volante desgastado)
-0.49::r :- str(dry), flw.
-0.01::r :- str(dry), \+flw.
-0.69::r :- str(wet), flw.
-0.31::r :- str(wet), \+flw.
-0.49::r :- str(snow_covered), flw.
-0.86::r :- str(snow_covered), \+flw.
-% Probabilidades condicionais de V (voltagem) dada R (dínamo deslizante)
-0.02::v :- r.   
-0.96::v :- \+r.
-% definindo as probabilidades das outras variáveis
-% B (lâmpada ok) e K (cabo ok)
-0.98::b.
-0.19::k.
-% Probabilidades finais da luz ligada (Li) dadas V, B, e K influenciando diretamente Li.
-0.99::li :- v, b, k.
-0.01::li :- v, b, \+k.
-0.01::li :- v, \+b, k.
-0.001::li :- v, \+b, \+k.
-0.3::li :- \+v, b, k.
-0.005::li :- \+v, b, \+k.
-0.005::li :- \+v, \+b, k.
-0.0::li :- \+v, \+b, \+k.
-% Evidência de que a condição da rua é snow_covered
+0.97::b.  % Probabilidade de a lâmpada estar ok (b)
+0.90::k.  % Probabilidade de o cabo estar ok (k)
+
+% Definindo as probabilidades condicionais para a luz ligada (li)
+0.99::li :- v, b, k.         
+0.01::li :- v, b, \+k.        
+0.01::li :- v, \+b, k.         
+0.001::li :- v, \+b, \+k.        
+0.3::li :- \+v, b, k.      
+0.005::li :- \+v, b, \+k.     
+0.005::li :- \+v, \+b, k.   
+0.0::li :- \+v, \+b, \+k.    
+
+% A variável str (condição da rua) pode ter três estados:
+% dry (seca), wet (molhada) ou snow_covered (coberta de neve)
+0.85::str(dry).            
+0.10::str(wet).             
+0.05::str(snow_covered).  
+
+% A variável flw indica se o volante do dínamo está desgastado ou não
+0.10::flw. % A probabilidade complementar (0.90) é implícita
+
+% Definição das probabilidades condicionais para a variável r (dínamo deslizante)
+% Dada a condição da rua (str) e o estado do volante (flw)
+0.10::r :- str(dry), flw.           
+0.01::r :- str(dry), \+flw.           
+0.55::r :- str(wet), flw.     
+0.20::r :- str(wet), \+flw.        
+0.80::r :- str(snow_covered), flw.  
+0.60::r :- str(snow_covered), \+flw. 
+
+% Definição das probabilidades condicionais para a variável v (voltagem)
+% Dada a condição do dínamo deslizante (r)
+0.80::v :- r.        
+0.20::v :- \+r.    
+
+% Evidência observada: A condição da rua é "snow_covered"
 evidence(str(snow_covered)).
 
-% Consultas para calcular a probabilidade de V (voltagem)
-query(v).
+% Consulta para calcular a probabilidade de v (voltagem)
+query(v). 
 
-% teremos então como resposta, a probabilidade de V, dado que a rua está coberta de neve.
+% Resultado esperado: A probabilidade de v, dado que a rua está coberta de neve
